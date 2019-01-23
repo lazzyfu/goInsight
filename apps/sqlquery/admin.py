cf1bcd0399450dc268e597d6d9800297709262dc
zongfei.fu <zongfei.fu@yunzhanghu.com>
2019-01-23 18:18:00 +0800

    修复后台无法删除主机的bug

--- apps/sqlquery/admin.py
+++ apps/sqlquery/admin.py
@@ -29,15 +29,6 @@ class MysqlRulesChainAdmin(admin.ModelAdmin):
             del actions['delete_selected']
         return actions
 
-    def has_add_permission(self, request):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
-
-    def has_change_permission(self, request, obj=None):
-        return False
-
 
 class MysqlRulesGroupAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_at')
