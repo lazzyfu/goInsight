7053ac1d6ce30a452903f88a8e93575202ed168d
zongfei.fu <zongfei.fu@yunzhanghu.com>
2020-10-15 15:11:11 +0800

    202010151450

--- yasql-fe/vue.config.js
+++ yasql-fe/vue.config.js
@@ -55,24 +55,25 @@ const vueConfig = {
   },
 
   chainWebpack: (config) => {
+    config.plugins.delete('prefetch')
     config.resolve.alias
       .set('@$', resolve('src'))
 
     const svgRule = config.module.rule('svg')
-    svgRule.uses.clear()
-    svgRule
-      .oneOf('inline')
-      .resourceQuery(/inline/)
-      .use('vue-svg-icon-loader')
-      .loader('vue-svg-icon-loader')
-      .end()
-      .end()
-      .oneOf('external')
-      .use('file-loader')
-      .loader('file-loader')
-      .options({
-        name: 'assets/[name].[hash:8].[ext]'
-      })
+    // svgRule.uses.clear()
+    // svgRule
+    //   .oneOf('inline')
+    //   .resourceQuery(/inline/)
+    //   .use('vue-svg-icon-loader')
+    //   .loader('vue-svg-icon-loader')
+    //   .end()
+    //   .end()
+    //   .oneOf('external')
+    //   .use('file-loader')
+    //   .loader('file-loader')
+    //   .options({
+    //     name: 'assets/[name].[hash:8].[ext]'
+    //   })
 
     // if prod is on
     // assets require on cdn
@@ -83,6 +84,8 @@ const vueConfig = {
       })
     }
   },
+
+  assetsDir: 'static',
 
   css: {
     loaderOptions: {
