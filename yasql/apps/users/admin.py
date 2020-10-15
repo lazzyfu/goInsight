cad855a0fafc43b90d5eae11c3e7f94e81b4dc81
zongfei.fu <zongfei.fu@yunzhanghu.com>
2020-10-15 12:05:43 +0800

    202010141845

--- yasql/apps/users/admin.py
+++ yasql/apps/users/admin.py
@@ -69,14 +69,14 @@ class RolePermissionAdmin(admin.ModelAdmin):
     )
     list_display_links = ('permission_desc',)
 
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
-    #
-    # def has_add_permission(self, request):
-    #     return False
-    #
-    # def has_change_permission(self, request, obj=None):
-    #     return False
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
 
 admin.site.register(models.UserAccounts, UserAccountsAdmin)
