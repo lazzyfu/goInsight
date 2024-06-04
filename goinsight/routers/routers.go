ae8bdf98d515a544898dcd5d30abed88a6a514ad
xff <wolfefu@gmail.com>
2024-06-04 18:05:16 +0800

    优化日志

--- goinsight/routers/routers.go
+++ goinsight/routers/routers.go
@@ -7,6 +7,7 @@ package routers
 
 import (
 	"goInsight/middleware"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/requestid"
@@ -25,12 +26,17 @@ func Include(opts ...Option) {
 // 初始化
 func Init() *gin.Engine {
 	r := gin.New()
+
 	// 使用CORS中间件
 	r.Use(cors.Default())
+
 	// 使用requestid
 	r.Use(requestid.New())
-	// 将http请求记录到文件
-	r.Use(middleware.LoggerRequestToFile())
+
+	// 初始化请求日志记录器
+	requestLogger := middleware.InitLogger(time.Now().Format("2006-01-02") + "-request.log")
+	r.Use(middleware.LoggerRequestToFile(requestLogger))
+
 	for _, opt := range options {
 		opt(r)
 	}
