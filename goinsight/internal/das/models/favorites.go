942618929220da2d53bbeabb3178affd6b3fb5d5
zongfei.fu <1126227133@qq.com>
2024-07-10 16:27:19 +0800

    修复dms收藏SQL唯一索引问题

--- goinsight/internal/das/models/favorites.go
+++ goinsight/internal/das/models/favorites.go
@@ -6,8 +6,8 @@ import (
 
 type InsightDASFavorites struct {
 	*models.Model
-	Username string `gorm:"type:varchar(128);not null;comment:执行的用户;index:idx_username" json:"username"`
-	Title    string `gorm:"type:varchar(256);not null;default:'';comment:标题;uniqueIndex" json:"title"`
+	Username string `gorm:"type:varchar(128);not null;comment:执行的用户;uniqueIndex:uniq_username_title" json:"username"`
+	Title    string `gorm:"type:varchar(256);not null;default:'';comment:标题;uniqueIndex:uniq_username_title" json:"title"`
 	Sqltext  string `gorm:"type:text;null;default:null;comment:用户收藏的SQL" json:"sqltext"`
 }
 
