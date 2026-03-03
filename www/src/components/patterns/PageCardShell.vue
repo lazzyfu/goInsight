4b725ee8228116e1641c1bac72b5d64f548d5fe0
Xff <1126227133@qq.com>
2026-03-03 22:53:36 +0800

    feat: add reusable admin and das page patterns

---
+++ www/src/components/patterns/PageCardShell.vue
@@ -0,0 +1,23 @@
+<template>
+  <a-card class="gi-page-card-shell" :title="title" :bordered="bordered">
+    <template v-if="$slots.extra" #extra>
+      <slot name="extra" />
+    </template>
+    <slot />
+  </a-card>
+</template>
+
+<script setup>
+defineOptions({ name: 'PageCardShell' })
+
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  bordered: {
+    type: Boolean,
+    default: true,
+  },
+})
+</script>
---
+++ www/src/components/patterns/PageToolbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="gi-page-toolbar gi-pattern-toolbar">
+    <div class="gi-page-toolbar-left">
+      <slot name="left">
+        <slot />
+      </slot>
+    </div>
+    <div v-if="$slots.right" class="gi-page-toolbar-right">
+      <slot name="right" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineOptions({ name: 'PageToolbar' })
+</script>
